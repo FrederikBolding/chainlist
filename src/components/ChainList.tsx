1b1c87425d4f6cf71aa1074573aa28276b54e783
Frederik Bolding <frederik.bolding@gmail.com>
2022-03-04 22:02:02 +0100

    Fix grid

--- src/components/ChainList.tsx
+++ src/components/ChainList.tsx
@@ -1,15 +1,15 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { GridItem, SimpleGrid } from "@chakra-ui/react";
 import React from "react";
 import { Chain } from "./Chain";
 
 export const ChainList = ({ chains }) => (
   <>
-    <Grid templateColumns="repeat(4, 1fr)" gap={3}>
+    <SimpleGrid columns={3} gap={4}>
       {chains.map((c) => (
         <GridItem key={c.id}>
           <Chain {...c} />
         </GridItem>
       ))}
-    </Grid>
+    </SimpleGrid>
   </>
 );
