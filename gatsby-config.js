1f28b98fb33e54215b2f8abe13ae9137c0b0080c
Frederik Bolding <frederik.bolding@gmail.com>
2022-03-04 20:00:11 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet"]
+};
