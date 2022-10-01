9de133616f1fad1e56f37c2a8bb91f46c60d4e15
Frederik Bolding <frederik.bolding@gmail.com>
2022-10-01 12:36:21 +0200

    Fix

--- scripts/check-deploy.sh
+++ scripts/check-deploy.sh
@@ -2,7 +2,7 @@
 shouldDeploy=$(curl -s https://api.github.com/repos/ethereum-lists/chains/commits/master | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
 
 echo $shouldDeploy
-if [ shouldDeploy = "0" ]
+if [ $shouldDeploy -eq 0 ]
 then
   echo "Deploying..."
   curl -X POST -d '{}' https://api.netlify.com/build_hooks/${BUILD_HOOK}
