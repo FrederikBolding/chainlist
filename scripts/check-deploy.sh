7cff671dd96b02ca5775cf9c9696d7eeec7a3b0d
Frederik Bolding <frederik.bolding@gmail.com>
2022-10-01 12:37:30 +0200

    Remove debug

--- scripts/check-deploy.sh
+++ scripts/check-deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 shouldDeploy=$(curl -s https://api.github.com/repos/ethereum-lists/chains/commits/master | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
 
-echo $shouldDeploy
 if [ $shouldDeploy -eq 0 ]
 then
   echo "Deploying..."
