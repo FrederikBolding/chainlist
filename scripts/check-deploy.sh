236b36ff5495a5324f1707b9128c187bb4d83284
Frederik Bolding <frederik.bolding@gmail.com>
2022-10-01 12:31:16 +0200

    debug

--- scripts/check-deploy.sh
+++ scripts/check-deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 shouldDeploy=$(curl -s https://api.github.com/repos/ethereum-lists/chains/commits/master | jq -r "((now - (.commit.author.date | fromdateiso8601) )  / (60*60*24)  | trunc)")
 
+echo $shouldDeploy
 if [ shouldDeploy = "0" ]
 then
+  echo "Deploying..."
   curl -X POST -d '{}' https://api.netlify.com/build_hooks/${BUILD_HOOK}
 fi
