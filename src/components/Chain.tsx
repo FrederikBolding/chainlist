f62657162623ed9f81ae87b4703bcf9758a87835
Frederik Bolding <frederik.bolding@gmail.com>
2023-01-23 16:23:32 +0100

    Fix some styling issues with images

--- src/components/Chain.tsx
+++ src/components/Chain.tsx
@@ -27,29 +27,44 @@ export const Chain = ({
     handleAddChain({ name, chainId, nativeCurrency, ...rest });
   };
   return (
-    <Flex flexDirection="column" px="5" py="4" borderWidth="1px" rounded="md" boxShadow="base">
-      <Flex mb="2" justifyContent="space-between">
-        <Text
-          fontSize="lg"
-          fontWeight="semibold"
-          lineHeight="short"
-          isTruncated
-          verticalAlign="middle"
-        >
-          {name}
-        </Text>
-        {icon && <GatsbyImage image={icon} alt={name} />}
+    <Flex
+      flexDirection="column"
+      px="5"
+      py="4"
+      borderWidth="1px"
+      rounded="md"
+      boxShadow="base"
+    >
+      <Flex justifyContent="space-between">
+        <Flex flexDirection="column" minW="200px">
+          <Flex mb="2">
+            <Text
+              fontSize="lg"
+              fontWeight="semibold"
+              lineHeight="short"
+              isTruncated
+              verticalAlign="middle"
+            >
+              {name}
+            </Text>
+          </Flex>
+          <StatGroup mb="2">
+            <Stat>
+              <StatLabel>Chain ID</StatLabel>
+              <StatNumber fontSize="md">{chainId}</StatNumber>
+            </Stat>
+            <Stat>
+              <StatLabel>Currency</StatLabel>
+              <StatNumber fontSize="md">{nativeCurrency.symbol}</StatNumber>
+            </Stat>
+          </StatGroup>
+        </Flex>
+        {icon && (
+          <Flex>
+            <GatsbyImage objectFit="scale-down" image={icon} alt={name} />
+          </Flex>
+        )}
       </Flex>
-      <StatGroup mb="2">
-        <Stat>
-          <StatLabel>Chain ID</StatLabel>
-          <StatNumber fontSize="md">{chainId}</StatNumber>
-        </Stat>
-        <Stat>
-          <StatLabel>Currency</StatLabel>
-          <StatNumber fontSize="md">{nativeCurrency.symbol}</StatNumber>
-        </Stat>
-      </StatGroup>
       <Center mt="auto">
         {!isConnected ? (
           <Button onClick={handleConnect}>Connect Wallet</Button>
