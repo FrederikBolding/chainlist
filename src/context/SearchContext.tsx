49e4b4d9bbf2d25267e253f241e3c6f1470a7f29
Frederik Bolding <frederik.bolding@gmail.com>
2023-09-02 23:04:05 +0200

    Persist filters

--- src/context/SearchContext.tsx
+++ src/context/SearchContext.tsx
@@ -1,11 +1,34 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const SearchContext = createContext({});
 
+const LOCALSTORAGE_KEY = "filters";
+
 export const SearchProvider = ({ children }) => {
+  const loadPersisted = () => {
+    try {
+      const result = window.localStorage.getItem(LOCALSTORAGE_KEY);
+      if (result) {
+        return JSON.parse(result);
+      }
+    } catch {}
+    return null;
+  };
+
+  const defaults = loadPersisted();
+
   const [query, setQuery] = useState("");
-  const [showTestnets, setShowTestnets] = useState(false);
-  const [showDeprecated, setShowDeprecated] = useState(false);
+  const [showTestnets, setShowTestnets] = useState(
+    defaults?.showTestnets ?? false
+  );
+  const [showDeprecated, setShowDeprecated] = useState(
+    defaults?.showDeprecated ?? false
+  );
+
+  useEffect(() => {
+    const str = JSON.stringify({ showTestnets, showDeprecated });
+    window.localStorage.setItem(LOCALSTORAGE_KEY, str);
+  }, [showTestnets, showDeprecated]);
 
   return (
     <SearchContext.Provider
@@ -15,7 +38,7 @@ export const SearchProvider = ({ children }) => {
         showTestnets,
         setShowTestnets,
         showDeprecated,
-        setShowDeprecated
+        setShowDeprecated,
       }}
     >
       {children}
