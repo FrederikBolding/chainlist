e79898c2ce998532aaf49f6327ffaf3111d4e2a1
Frederik Bolding <frederik.bolding@gmail.com>
2024-01-08 00:11:26 +0100

    Experiment with chain pages

--- src/components/Chain.tsx
+++ src/components/Chain.tsx
@@ -12,6 +12,7 @@ import React, { useContext } from "react";
 import { Web3Context } from "../context/Web3Context";
 import { ChainData } from "../types/chain";
 import { ChainIcon } from "./ChainIcon";
+import { Link } from "gatsby";
 
 export const Chain = ({
   name,
@@ -26,51 +27,53 @@ export const Chain = ({
     handleAddChain({ name, chainId, nativeCurrency, ...rest });
   };
   return (
-    <Flex
-      flexDirection="column"
-      px="5"
-      py="4"
-      borderWidth="1px"
-      rounded="md"
-      boxShadow="base"
-    >
-      <Flex justifyContent="space-between">
-        <Flex flexDirection="column" minW="200px">
-          <Flex mb="2">
-            <Text
-              fontSize="lg"
-              fontWeight="semibold"
-              lineHeight="short"
-              isTruncated
-              verticalAlign="middle"
-            >
-              {name}
-            </Text>
+    <Link to={`/chain/${chainId}`}>
+      <Flex
+        flexDirection="column"
+        px="5"
+        py="4"
+        borderWidth="1px"
+        rounded="md"
+        boxShadow="base"
+      >
+        <Flex justifyContent="space-between">
+          <Flex flexDirection="column" minW="200px">
+            <Flex mb="2">
+              <Text
+                fontSize="lg"
+                fontWeight="semibold"
+                lineHeight="short"
+                isTruncated
+                verticalAlign="middle"
+              >
+                {name}
+              </Text>
+            </Flex>
+            <StatGroup mb="2">
+              <Stat>
+                <StatLabel>Chain ID</StatLabel>
+                <StatNumber fontSize="md">{chainId}</StatNumber>
+              </Stat>
+              <Stat>
+                <StatLabel>Currency</StatLabel>
+                <StatNumber fontSize="md">{nativeCurrency.symbol}</StatNumber>
+              </Stat>
+            </StatGroup>
           </Flex>
-          <StatGroup mb="2">
-            <Stat>
-              <StatLabel>Chain ID</StatLabel>
-              <StatNumber fontSize="md">{chainId}</StatNumber>
-            </Stat>
-            <Stat>
-              <StatLabel>Currency</StatLabel>
-              <StatNumber fontSize="md">{nativeCurrency.symbol}</StatNumber>
-            </Stat>
-          </StatGroup>
+          {icon && (
+            <Flex>
+              <ChainIcon name={name} icon={icon} />
+            </Flex>
+          )}
         </Flex>
-        {icon && (
-          <Flex>
-            <ChainIcon name={name} icon={icon} />
-          </Flex>
-        )}
+        <Center mt="auto">
+          {!isConnected ? (
+            <Button onClick={handleConnect}>Connect Wallet</Button>
+          ) : (
+            <Button onClick={handleAddChainClick}>Add Chain</Button>
+          )}
+        </Center>
       </Flex>
-      <Center mt="auto">
-        {!isConnected ? (
-          <Button onClick={handleConnect}>Connect Wallet</Button>
-        ) : (
-          <Button onClick={handleAddChainClick}>Add Chain</Button>
-        )}
-      </Center>
-    </Flex>
+    </Link>
   );
 };
